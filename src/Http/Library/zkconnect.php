c0eb58eb30d756941afdf20ff83dd6542965a7c5
Rezaul Islam <rejaulcuet88@gmail.com>
2022-05-22 02:29:08 +0600

    Update timeout issue

--- src/Http/Library/zkconnect.php
+++ src/Http/Library/zkconnect.php
@@ -1,11 +1,11 @@
 <?php
 function zkconnect($self)
 {
-    $command = CMD_CONNECT;
+    $command = 1000;
     $command_string = '';
     $chksum = 0;
     $session_id = 0;
-    $reply_id = -1 + USHRT_MAX;
+    $reply_id = -1 + 65535;
 
     $buf = $self->createHeader($command, $chksum, $session_id, $reply_id, $command_string);
 
